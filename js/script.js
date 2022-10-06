b94306d2de3dc4bc120441bb1fd620f0c6f3dc2e
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-10-06 18:47:25 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@
  * Do basic math.
  */
 function CalculateClicked() {
-    document.getElementById("area").innerHTML = "<p>The area is:" + (10 * 6) + "cm²." + "</p>"
-    document.getElementById("perimeter").innerHTML = "<p>The perimeter is:" + (2 * (10 + 6)) + "cm." + "</p>"
+  document.getElementById("area").innerHTML =
+    "<p>The area is:" + 10 * 6 + "cm²." + "</p>"
+  document.getElementById("perimeter").innerHTML =
+    "<p>The perimeter is:" + 2 * (10 + 6) + "cm." + "</p>"
 }
