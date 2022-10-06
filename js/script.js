32575f3cc0fa8a1cdaf7957c015fd2ff18a3ca7a
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-06 14:45:33 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 liya getachew All rights reserved
+//
+// Created by: liya getachew
+// Created on: Oct 6 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function CalculateClicked() {
+    document.getElementById("area").innerHTML = "<p>The area is:" + (10 * 6) + "cm²." + "</p>"
+    document.getElementById("perimeter").innerHTML = "<p>The perimeter is:" + (2 * (10 + 6)) + "cm." + "</p>"
+}
